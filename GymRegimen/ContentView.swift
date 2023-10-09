c2dcedd2910a44907845a6e44c8e9f90496adc0a
lch27 <mag.anderson96@gmail.com>
2023-10-09 17:29:19 +0900

    Initial Commit

---
+++ GymRegimen/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GymRegimen
+//
+//  Created by Maggie Li on 2023/10/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GymRegimen/GymRegimenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymRegimenApp.swift
+//  GymRegimen
+//
+//  Created by Maggie Li on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct GymRegimenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
